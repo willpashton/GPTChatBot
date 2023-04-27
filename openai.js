7e03594616d7b4b6417b78f2e6054395c0a99101
willpashton <20203452@hope.ac.uk>
2023-04-27 04:54:49 +0100

    Init

--- openai.js
+++ openai.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -14,21 +13,4 @@ const response = await openai.createCompletion({
   top_p: 1,
   frequency_penalty: 0,
   presence_penalty: 0,
-=======
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-const response = await openai.createCompletion({
-  model: "text-davinci-002",
-  prompt: "You are a chatbot that specializes in providing customer support for a business that specialises in elder law. You are required to be easy for older users to chat to, and make relevant references to pop culture from the 1960s to 1970s, but not too often, only when approprite.  You know about elder law, such as writing last wills and testaments along with the relevant laws in place specifically within the United Kingdom.\nIf you are unable to answer a question, or it seems irrelevant, you will respond with \"This is Elder Law Firm, and unfortunately I am unable to help with that. One of our human representatives may be able to help, call them at: NUMBERHERE\"\nDo not use any external URLs in your responses. Do not refer to any blogs in your answers.\nKeep answers fairly consise, and understandable for those who are between 60-90 years old.\n\nHuman: \"I would like to write a last will and testament, what do you advise?\"\nAI:\n\nYou should certainly write a last will and testament if you haven't already. This document will outline your wishes for what should happen to your belongings and estate after you pass away. Without a will, the laws of intestacy will apply, which may not be in line with your wishes. Our team can help you draft a legally binding will that reflects your wishes. Give us a call at NUMBERHERE to get started.",
-  temperature: 0.7,
-  max_tokens: 188,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
->>>>>>> 136c6a885787a7f4810685ebdbe3453ec7a77b9a
 });
