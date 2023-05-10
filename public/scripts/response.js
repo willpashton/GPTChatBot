a3bc7244ca37caa757c1e6014a1c6275a80e60d5
willpashton <20203452@hope.ac.uk>
2023-05-10 04:03:36 +0100

    dictionary for basic chatbot

--- public/scripts/response.js
+++ public/scripts/response.js
@@ -17,14 +17,27 @@ function hardCodedResponse(input){
     }
     processInput = input.toLowerCase();
     //hard coded responses
-
-    if(processInput == "hello"){
-        return "Hello! What can I help you with today?";
-    }
-    else if (processInput == "goodbye"){
-        return "Goodbye! I hope I have been helpful!"
-    }
-    else{
-        return "Sorry, I can't help you with that, Contacting one of our human agents now..."
+    var responseDict = {
+        "hello" : "Hello! What can I help you with today?",
+        "goodbye" : "Goodbye! I hope I have been helpful!",
+        "will" : "We are able to write last wills and testaments. It's very important to write these documents so then your family is able to carry out your wishes. If you call us on (0800123123) then we can guide you through the process.",
+        "neglect" : "We are sorry to hear that you have to enquire about this. We are specialists in representing elder abuse, neglect and fraud cases - and have a high success rate as prosecutors in this area. To begin opening a case with us, call us on (0800123123)",
+        "fraud" : "We are sorry to hear that you have to enquire about this. We are specialists in representing elder abuse, neglect and fraud cases - and have a high success rate as prosecutors in this area. To begin opening a case with us, call us on (0800123123)",
+        "abuse" : "We are sorry to hear that you have to enquire about this. We are specialists in representing elder abuse, neglect and fraud cases - and have a high success rate as prosecutors in this area. To begin opening a case with us, call us on (0800123123)",
+        "retirement" : "We are able to provide many resources for retirement planning! We congratulate you on your decision, and suggest you give us a call on (0800123123) so we can discuss next steps.",
+        "end of life" : "We are one of the highest rated companies in the UK for end-of-life planning, call us on (0800123123) where we can lay out the next steps you need to take.",
+    };
+    if(processInput != "blank"){
+        const words = processInput.split(" ");
+        var result = "";
+        words.forEach(word => {
+          if (responseDict.hasOwnProperty(word)) {
+            result = responseDict[word];
+          }
+          else{
+            result = "Sorry, I can't help you with that, Contacting one of our human agents now...\n Alternatively, call us on (0800123123).";
+          }
+        });
+        return result;
     }
 }
